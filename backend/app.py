6688f0031159bc00e0657d6c3b1171d40be57b6a
Beatrice Yap <beatriceyapsm@gmail.com>
2022-11-17 12:30:55 +0800

    update app

---
+++ backend/app.py
@@ -0,0 +1,96 @@
+import streamlit as st  # pip install streamlit
+import pandas as pd  # pip install pandas
+import plotly.express as px  # pip install plotly-express
+import base64  # Standard Python Module
+from io import StringIO, BytesIO  # Standard Python Module
+
+# import pmdarima as pm #pip install pmdarima
+# from pmdarima.model_selection import train_test_split #for forecasting
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import matplotlib
+# matplotlib.use('TkAgg')
+
+import prophet
+from prophet import Prophet
+
+# def generate_excel_download_link(df):
+#     # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
+#     towrite = BytesIO()
+#     df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+#     towrite.seek(0)  # reset pointer
+#     b64 = base64.b64encode(towrite.read()).decode()
+#     href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
+#     return st.markdown(href, unsafe_allow_html=True)
+
+# def generate_html_download_link(fig):
+#     # Credit Plotly: https://discuss.streamlit.io/t/download-plotly-plot-as-html/4426/2
+#     towrite = StringIO()
+#     fig.write_html(towrite, include_plotlyjs="cdn")
+#     towrite = BytesIO(towrite.getvalue().encode())
+#     b64 = base64.b64encode(towrite.read()).decode()
+#     href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
+#     return st.markdown(href, unsafe_allow_html=True)
+
+
+st.set_page_config(page_title='Excel Plotter')
+st.title('Excel Plotter 📈')
+st.subheader('Feed me with your Excel file')
+
+uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    # df = df.sort_values(by='Order Date', ascending=True)
+    # st.dataframe(df)
+    # groupby_column = st.selectbox(
+    #     'What would you like to analyse?',
+    #     ('Order Date', 'Month', 'Year'),
+    # )
+ 
+    # -- GROUP DATAFRAME
+    # output_columns = ['Quantity']
+    # df_grouped = df.groupby(by=[groupby_column], as_index=False).agg({'Quantity':'sum'})
+
+    # -- FORECASTING
+    # train, test = train_test_split(df, train_size=round(len(df)*0.8,None))
+    # sns.set()
+    # # train = btc[btc.index < pd.to_datetime("2020-11-01", format='%Y-%m-%d')]
+    # # test = btc[btc.index > pd.to_datetime("2020-11-01", format='%Y-%m-%d')]
+
+    # plt.plot(train, color = "black")
+    # plt.plot(test, color = "red")
+    # plt.ylabel('Sales')
+    # plt.xlabel('Date')
+    # plt.xticks(df['Order Date'])
+    # plt.title("Train/Test split for Sales Data")
+    # plt.show()
+
+    m = Prophet()
+    m.fit(df)
+    future = m.make_future_dataframe(periods=6, freq='M')
+    future = future.tail(6)
+    # st.dataframe(future)
+    forecast = m.predict(future)
+    # forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+    st.dataframe(forecast)
+    fig1 = m.plot(forecast, xlabel='Date', ylabel='Sales Quantity')
+    # fig2 = m.plot_components(forecast)
+
+    st.write(fig1)
+
+    # -- PLOT DATAFRAME
+    # fig = px.line(
+    #     df_grouped,
+    #     x=groupby_column,
+    #     y='Quantity',
+    #     # color='Category',
+    #     # template='plotly_white',
+    #     title=f'<b>Sales by {groupby_column}</b>'
+    # )
+    # st.plotly_chart(fig)
+
+    # -- DOWNLOAD SECTION
+    # st.subheader('Downloads:')
+    # generate_excel_download_link(df_grouped)
+    # generate_html_download_link(fig)
--- backend/main.js
+++ backend/main.js
@@ -12,6 +12,32 @@ app.use(body_parser.json());
 app.use(api.router);//ask app to use router
 //app.use(authdemo.router);
 
+app.get('/', (req,res) => {
+    res.send("Hello World");});
+
+app.post("/readPython", (request, response) => {
+    // Reading Python files
+    var dataToSend;
+    // spawn new child process to call the python script
+    const python = spawn('python3', ['app.py', "hi", "Duyen"]);
+  
+       // collect data from script
+    python.stdout.on('data', function (data) {
+    dataToSend = data.toString();
+    });
+  
+    python.stderr.on('data', data => {
+    console.error(`stderr: ${data}`);
+    });
+  
+       // in close event we are sure that stream from child process is closed
+    python.on('exit', (code) => {
+    console.log(`child process exited with code ${code}, ${dataToSend}`);
+    response.sendFile(`${__dirname}/public/result.html`);
+    }); 
+  });
+
+
 app.listen(process.env.PORT || 3000, (error)=> {
     if (error) {
         console.log(error);
